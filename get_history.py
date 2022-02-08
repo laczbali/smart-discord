cfd722db5079e2524931e6f492669f9b7647b007
Balazs Laczko <laczbali@gmail.com>
2022-02-08 18:49:36 +0100

    fix: edited output format of get_history so that it is more easily parsable

--- get_history.py
+++ get_history.py
@@ -1,6 +1,7 @@
 # This script can be used to download chat history,
 # so that the data can be used for training a custom model
 
+import json
 import discord
 import os
 from dotenv import load_dotenv
@@ -9,7 +10,7 @@ load_dotenv()
 
 # -----------------------------------------------------------------------------
 GUILD_NAME = "" # Name of the guild, which you want to download
-MESSAGE_LIMIT_PER_CHANNE = 500 # Number of messages to download per channel
+MESSAGE_LIMIT_PER_CHANNE = 1000 # Number of messages to download per channel
 REPLACE_NAME = "" # name of a user, which you want to train the bot to respond as
 BOT_NAME = os.environ.get('BOT_NAME')
 # -----------------------------------------------------------------------------
@@ -38,34 +39,35 @@ async def on_ready():
         ch_messages = []
 
         for m in history:
+            author = m.author.name
             content = m.content
 
             # replace mention IDs with names
             for mention in m.mentions:
                 content = content.replace(f"<@!{mention.id}>", mention.name)
 
-            # format message
-            out = f"{m.author.name}: {content}"
-
             # replace the name of a user with the name of the bot
             # so that the prompts later are lined up with the training data
             if REPLACE_NAME != "":
-                out = out.replace(REPLACE_NAME, BOT_NAME)
+                author = author.replace(REPLACE_NAME, BOT_NAME)
+                content = content.replace(REPLACE_NAME, BOT_NAME)
+
+            # build output
+            out = [author, content]
 
             # return
             ch_messages.append(out)
 
         # save channel messages to file
-        with open(f"{channel.name}.history.txt", "w", encoding="utf-8") as f:
-            for m in ch_messages:
-                f.write(f"{m}\n")
+        # with open(f"{channel.name}.history.txt", "w", encoding="utf-8") as f:
+        #     for m in ch_messages:
+        #         f.write(f"{m}\n")
 
         all_messages.extend(ch_messages)
 
     # save all messages to file
-    with open("all.history.txt", "w", encoding="utf-8") as f:
-        for m in all_messages:
-            f.write(f"{m}\n")
+    with open("all.history.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(all_messages))
 
     print("done")
 
